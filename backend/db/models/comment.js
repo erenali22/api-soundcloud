3768bac88094df5df67940956fccb371ee83a388
erenali22 <erena.li@outlook.com>
2022-11-28 12:54:30 -0800

    comment

--- backend/db/models/comment.js
+++ backend/db/models/comment.js
@@ -1,20 +1,22 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Comment extends Model {
     static associate(models) {
-      Comment.belongsTo(models.User, { foreignKey: "userId" })
+      Comment.belongsTo(models.Song, { foreignKey: "songId" });
+      Comment.belongsTo(models.User, { foreignKey: "userId" });
     }
   }
-  Comment.init({
-    userId: DataTypes.INTEGER,
-    songId: DataTypes.INTEGER,
-    body: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Comment',
-  });
+  Comment.init(
+    {
+      userId: DataTypes.INTEGER,
+      songId: DataTypes.INTEGER,
+      body: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Comment",
+    },
+  );
   return Comment;
 };
