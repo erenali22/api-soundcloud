e71c840d6aa377a23ed82f7bc80937f4f7be7866
erenali22 <erena.li@outlook.com>
2022-11-27 21:21:04 -0800

    fix playlist

--- backend/db/models/playlist.js
+++ backend/db/models/playlist.js
@@ -1,20 +1,25 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Playlist extends Model {
     static associate(models) {
-      Playlist.belongsToMany(models.Song, { through: models.PlaylistSong, foreignKey: "playlistId" })
+      Playlist.belongsToMany(models.Song, {
+        through: models.PlaylistSong,
+        foreignKey: "playlistId",
+      });
+      Playlist.belongsTo(models.User, { foreignKey: "userId" });
     }
   }
-  Playlist.init({
-    userId: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    imageUrl: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Playlist',
-  });
+  Playlist.init(
+    {
+      userId: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+      imageUrl: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Playlist",
+    },
+  );
   return Playlist;
 };
