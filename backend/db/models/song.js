5325cce6891258e71f2464a7f70fed59e331f384
erenali22 <erena.li@outlook.com>
2022-11-28 12:45:09 -0800

    albumId

--- backend/db/models/song.js
+++ backend/db/models/song.js
@@ -1,28 +1,38 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Song extends Model {
     static associate(models) {
-      Song.belongsToMany(models.Playlist, { through: models.PlaylistSong, as: "Songs", foreignKey: "songId", onDelete: 'CASCADE', hooks: true })
-      Song.hasMany(models.Comment, { foreignKey: "songId", onDelete: 'CASCADE', hooks: true })
-      Song.belongsTo(models.User, { foreignKey: "userId", as: "Artist" })
-      Song.belongsTo(models.Album, { foreignKey: "userId", as: "Album" })
+      Song.belongsToMany(models.Playlist, {
+        through: models.PlaylistSong,
+        as: "Songs",
+        foreignKey: "songId",
+        onDelete: "CASCADE",
+        hooks: true,
+      });
+      Song.hasMany(models.Comment, {
+        foreignKey: "songId",
+        onDelete: "CASCADE",
+        hooks: true,
+      });
+      Song.belongsTo(models.User, { foreignKey: "userId", as: "Artist" });
+      Song.belongsTo(models.Album, { foreignKey: "albumId", as: "Album" });
     }
   }
-  Song.init({
-    userId: DataTypes.INTEGER,
-    albumId: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    url: DataTypes.STRING,
-    imageUrl: DataTypes.STRING,
-  }, {
-    sequelize,
-    modelName: 'Song',
-  });
-
+  Song.init(
+    {
+      userId: DataTypes.INTEGER,
+      albumId: DataTypes.INTEGER,
+      title: DataTypes.STRING,
+      description: DataTypes.STRING,
+      url: DataTypes.STRING,
+      imageUrl: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Song",
+    },
+  );
 
   return Song;
 };
