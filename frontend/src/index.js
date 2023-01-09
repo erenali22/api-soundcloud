9d2cc4303fcd9a6aea426e4098868f5de093836b
erenali22 <erena.li@outlook.com>
2023-01-09 04:20:41 -0800

    Modal context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
+
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ModalProvider, Modal } from './context/Modal';
+import App from './App';
+
 import configureStore from './store';
-import { restoreCSRF, csrfFetch } from './store/csrf';
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from './store/session';
 
 const store = configureStore();
 
-if (process.env.NOVE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
-
 }
 
+// Wrap the application with the Modal provider and render the Modal component
+// after the App component so that all the Modal content will be layered as
+// HTML elements on top of the all the other HTML elements:
 function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
 
@@ -33,5 +42,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
