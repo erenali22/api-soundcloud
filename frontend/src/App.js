8063c86861d214678a0dee2a18f563814fad8435
erenali22 <erena.li@outlook.com>
2023-01-09 20:29:17 -0800

    Modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
-// frontend/src/App.js
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignupFormPage";
+import SignupFormModal from "./components/SignupFormModal";
+import LoginFormModal from "./components/LoginFormModal";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -19,11 +18,11 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
           <Route path="/signup">
-            <SignupFormPage />
+            <SignupFormModal />
+          </Route>
+          <Route path="/login">
+            <LoginFormModal />
           </Route>
         </Switch>
       )}
